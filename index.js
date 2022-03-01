3c716458d2385ba475aa8657b4fb58a97056bf78
Ashfaq Mangapatnam <AshfaqMangapatnam@gmail.com>
2022-03-01 10:21:19 +0530

    Add the controller and router

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-
+//use express router
+app.use('/',require('./routes'));
 app.listen(port,function(err)
 {
     if(err){
-        console.log('error in running server : ${err}');
+        console.log('error in running server',err);
     }
-    console.log('yupp!! server is running on port : ${port}');
+    console.log('yupp!! server is running on port : ${port}',port);
 });
